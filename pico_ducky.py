b8d315ff6c88f332e9bfb331eab457bb3206d179
judedanger2012 <judeprivett2012@gmail.com>
2025-11-01 12:57:41 -0600

    Add files via upload

---
+++ pico_ducky.py
@@ -0,0 +1,73 @@
+#!
+import keyboard
+import time
+
+i = 1
+
+while i == 1:
+    time.sleep(30)
+    i = 2
+while i == 2:
+    keyboard.press_and_release('ctrl+alt+t')
+    time.sleep(0.5)
+    i = 3
+while i == 3:
+    keyboard.write("chromium",delay= 0.5)
+    time.sleep(0.5)
+    i = 4
+while i == 4:
+    keyboard.press_and_release('enter')
+    time.sleep(0.5)
+    i = 5
+while i == 5:
+    keyboard.write("https://docs.google.com/document/u/0/",delay= 0.5)
+    time.sleep(0.5)
+    i = 6
+while i == 6:
+    keyboard.press_and_release('enter')
+    time.sleep(0.5)
+    i = 7
+while i == 7:
+    keyboard.write("Look keystroke injection is real you need to watch out if you look in your usba port you will see a",delay= 0.5)
+    time.sleep(0.5)
+    i = 8
+while i == 8:
+    keyboard.press_and_release('enter')
+    time.sleep(0.5)
+    i = 9
+while i == 9:
+    keyboard.write("usb that is what currently writes this message using keystroke injection to do it. if i were actually",delay= 0.5)
+    time.sleep(0.5)
+    i = 10
+while i == 10:
+    keyboard.press_and_release('enter')   
+    time.sleep(0.5) 
+    i = 11
+while i == 11:
+    keyboard.write("a bad hacker i would have compromised your system taking credentials and other sensitive data",delay= 0.5)
+    time.sleep(0.5)
+    keyboard.press_and_release('enter')
+    i = 12
+while i == 12:
+    time.sleep(0.5)
+    keyboard.write("im not but let this be a warning always check your usb ports also because you may not know what keystroke injection is",delay= 0.5)
+    keyboard.sleep(0.5)
+    i = 13
+while i == 13:
+    keyboard.press_and_release('enter')
+    time.sleep(0.5)
+    i = 14
+while i == 14:
+    keyboard.write("follow this link to learn more https://zsecurity.org/glossary/keystroke-injection-usb-or-cable/",delay= 0.5)
+    time.sleep(0.5)
+    i = 15
+while i == 15:
+    keyboard.press_and_release('enter')
+    time.sleep(0.5)
+    i = 16
+while i == 16:
+    keyboard.write("The specific keystroke injection technique i used was a usb that imitates a wired keyboard when its plugged in this link shows the exact usb i used to conduct this attack wich i state again was harmless and was only to spread awareness here is the device i used https://github.com/Outdatedcandy92/PicoDucky Now im happy to say i believe my goal was met and have spread awareness thank you very much for listening you can take the usb out now!!!",delay= 0.5)
+    time.sleep(0.5)
+    i = 17
+while i == 17:
+    break
